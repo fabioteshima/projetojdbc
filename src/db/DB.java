a88fb66d5578c08874757edbe29e7735b54c6b21
Fabio Teshima <fytrex@yahoo.com>
2025-11-20 16:47:20 -0300

    Pacote para conexão criado

---
+++ src/db/DB.java
@@ -0,0 +1,49 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn;
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties prop = new Properties();
+			prop.load(fs);
+			return prop;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties prop = loadProperties();
+				String url = prop.getProperty("dburl");
+				conn = DriverManager.getConnection(url, prop);
+				return conn;
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		else {
+			return conn;
+		}
+	}
+	
+	public static void closeConnection() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
+	
