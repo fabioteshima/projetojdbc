3fd7e217f9a93ae8f290f9de3fce6aa4acd0e02a
Fabio Teshima <fytrex@yahoo.com>
2025-11-20 17:28:41 -0300

    Criado Seller e Department. Criado SellerDao e DepartmentDao

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,16 +1,27 @@
 package apllication;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import db.DB;
+import model.db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		Connection conn = DB.getConnection();
 		DB.closeConnection();
 		
+		Department dep = new Department(1, "Vendas");
+		System.out.println(dep);
+		
+		Seller sel = new Seller(1, "Bob", "bob@gmail.com", LocalDate.parse("11/11/2011", dtf), 10000.0, dep);
+		System.out.println(sel);
 	}
 
 }
